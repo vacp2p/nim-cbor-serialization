7f03ca8c449dfa5357aaa63049faa12a12ed80a5
nitely <ecastroborsani@gmail.com>
2025-09-18 14:05:07 -0300

    wip

--- tests/test_all.nim
+++ tests/test_all.nim
@@ -9,8 +9,6 @@
 
 {.warning[UnusedImport]: off.}
 
-echo (NimMajor, NimMinor, NimPatch)
-
 import
   test_spec, test_serialization, test_simple_value, test_cbor_flavor, test_parser,
   test_reader, test_writer, test_valueref, test_cbor_raw, test_std
