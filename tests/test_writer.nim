ca4f5e2cf8bb7e3527078499887b6a6e6ebab98b
Esteban C Borsani <ecastroborsani@gmail.com>
2025-10-01 17:47:23 -0300

    Write text/byte-string in chunks and byte-by-byte (#6)

--- tests/test_writer.nim
+++ tests/test_writer.nim
@@ -8,7 +8,8 @@
 # those terms.
 
 import
-  unittest2,
+  pkg/unittest2,
+  pkg/stew/byteutils,
   ./utils,
   ../cbor_serialization/pkg/results,
   ../cbor_serialization/std/options,
@@ -252,3 +253,289 @@ suite "Test writer":
     let v = MyCbor.encode(One)
     check v.hex == "0x634f6e65"
     checkCbor v, "One"
+
+type
+  DefinText = distinct string
+  IndefText = distinct seq[string]
+  DefinBytes = distinct seq[byte]
+  IndefBytes = distinct seq[seq[byte]]
+  StringLikeObj = object
+    definText: DefinText
+    indefText: IndefText
+    definBytes: DefinBytes
+    indefBytes: IndefBytes
+    marker: int
+
+createCborFlavor StringLikeCbor,
+  automaticObjectSerialization = false, automaticPrimitivesSerialization = false
+
+proc writeValue*(
+    w: var StringLikeCbor.Writer, val: DefinText
+) {.gcsafe, raises: [IOError].} =
+  writeText(w, val.string.len):
+    for x in val.string:
+      w.writeChar(x)
+
+proc writeValue*(
+    w: var StringLikeCbor.Writer, val: IndefText
+) {.gcsafe, raises: [IOError].} =
+  writeText(w):
+    for chunk in seq[string](val):
+      writeText(w, chunk.len):
+        for x in chunk:
+          w.writeChar(x)
+
+proc writeValue*(
+    w: var StringLikeCbor.Writer, val: seq[string]
+) {.gcsafe, raises: [IOError].} =
+  writeText(w):
+    for chunk in val:
+      w.write(chunk)
+
+proc writeValue*(
+    w: var StringLikeCbor.Writer, val: seq[seq[string]]
+) {.gcsafe, raises: [IOError].} =
+  writeArray(w):
+    for s in val:
+      writeValue(w, s)
+
+proc writeValue*(
+    w: var StringLikeCbor.Writer, val: DefinBytes
+) {.gcsafe, raises: [IOError].} =
+  writeBytes(w, seq[byte](val).len):
+    for x in seq[byte](val):
+      w.writeByte(x)
+
+proc writeValue*(
+    w: var StringLikeCbor.Writer, val: IndefBytes
+) {.gcsafe, raises: [IOError].} =
+  writeBytes(w):
+    for chunk in seq[seq[byte]](val):
+      writeBytes(w, chunk.len):
+        for x in chunk:
+          w.writeByte(x)
+
+proc writeValue*(
+    w: var StringLikeCbor.Writer, val: seq[seq[byte]]
+) {.gcsafe, raises: [IOError].} =
+  writeBytes(w):
+    for chunk in val:
+      w.write(chunk)
+
+proc writeValue*(
+    w: var StringLikeCbor.Writer, val: seq[seq[seq[byte]]]
+) {.gcsafe, raises: [IOError].} =
+  writeArray(w):
+    for s in val:
+      writeValue(w, s)
+
+proc writevalue*(w: var StringLikeCbor.Writer, val: int) =
+  write(w, val)
+
+proc writevalue*(w: var StringLikeCbor.Writer, val: StringLikeObj) =
+  writeRecordValue(w, val)
+
+func toWriter(output: var OutputStream): CborWriter[DefaultFlavor] =
+  output = memoryOutput()
+  CborWriter[DefaultFlavor].init(output)
+
+suite "Test write text":
+  test "definite":
+    let cbor = StringLikeCbor.encode("abc".DefinText)
+    check cbor.hex == "0x63616263"
+    checkCbor cbor, "abc"
+
+  test "indefinite 1 chunk":
+    let cbor = StringLikeCbor.encode(@["abc"].IndefText)
+    check cbor.hex == "0x7f63616263ff"
+    checkCbor cbor, "abc"
+
+  test "indefinite 3 chunks":
+    let cbor = StringLikeCbor.encode(@["a", "bc", "def"].IndefText)
+    check cbor.hex == "0x7f616162626363646566ff"
+    checkCbor cbor, "abcdef"
+
+  test "indefinite with writeValue":
+    let cbor = StringLikeCbor.encode(@["a", "bc", "def"])
+    check cbor.hex == "0x7f616162626363646566ff"
+    checkCbor cbor, "abcdef"
+
+  test "array of indefinite text":
+    let cbor = StringLikeCbor.encode(@[@["a", "b"], @["c"]])
+    check cbor.hex == "0x9f7f61616162ff7f6163ffff"
+    checkCbor cbor, @["ab", "c"]
+
+  test "indefinite nesting not allowed":
+    var output: OutputStream
+    var w = toWriter output
+    writeText(w):
+      expect AssertionDefect:
+        writeText(w):
+          w.writeChar('a')
+
+  test "definite nesting not allowed":
+    var output: OutputStream
+    var w = toWriter output
+    writeText(w, 1):
+      expect AssertionDefect:
+        writeText(w, 1):
+          w.writeChar('a')
+
+  test "indefinite in definite not allowed":
+    var output: OutputStream
+    var w = toWriter output
+    writeText(w, 1):
+      expect AssertionDefect:
+        writeText(w):
+          w.writeChar('a')
+
+  test "bytes in indefinite text not allowed":
+    var output: OutputStream
+    var w = toWriter output
+    writeText(w):
+      expect AssertionDefect:
+        writeBytes(w, 1):
+          w.writeByte('a'.byte)
+
+  test "bytes in definite text not allowed":
+    var output: OutputStream
+    var w = toWriter output
+    writeText(w, 1):
+      expect AssertionDefect:
+        writeBytes(w, 1):
+          w.writeByte('a'.byte)
+
+  test "non-text in indefinite text not allowed":
+    var output: OutputStream
+    var w = toWriter output
+    writeText(w):
+      expect AssertionDefect:
+        w.write(123)
+
+  test "non-text in definite text not allowed":
+    var output: OutputStream
+    var w = toWriter output
+    writeText(w, 1):
+      expect AssertionDefect:
+        w.write(123)
+
+  test "text in definite text not allowed":
+    var output: OutputStream
+    var w = toWriter output
+    writeText(w, 1):
+      expect AssertionDefect:
+        w.write("a")
+
+suite "Test write byte-string":
+  test "definite":
+    let cbor = StringLikeCbor.encode("abc".toBytes.DefinBytes)
+    check cbor.hex == "0x43616263"
+    checkCbor cbor, "abc".toBytes
+
+  test "indefinite 1 chunk":
+    let cbor = StringLikeCbor.encode(@["abc".toBytes].IndefBytes)
+    check cbor.hex == "0x5f43616263ff"
+    checkCbor cbor, "abc".toBytes
+
+  test "indefinite 3 chunks":
+    let val = @["a".toBytes, "bc".toBytes, "def".toBytes]
+    let cbor = StringLikeCbor.encode(val.IndefBytes)
+    check cbor.hex == "0x5f416142626343646566ff"
+    checkCbor cbor, "abcdef".toBytes
+
+  test "indefinite with byte chunks":
+    let val = @["a".toBytes, "bc".toBytes, "def".toBytes]
+    let cbor = StringLikeCbor.encode(val)
+    check cbor.hex == "0x5f416142626343646566ff"
+    checkCbor cbor, "abcdef".toBytes
+
+  test "array of indefinite byte-strings":
+    let cbor = StringLikeCbor.encode(@[@["a".toBytes, "b".toBytes], @["c".toBytes]])
+    check cbor.hex == "0x9f5f41614162ff5f4163ffff"
+    checkCbor cbor, @["ab".toBytes, "c".toBytes]
+
+  test "indefinite nesting not allowed":
+    var output: OutputStream
+    var w = toWriter output
+    writeBytes(w):
+      expect AssertionDefect:
+        writeBytes(w):
+          w.writeByte('a'.byte)
+
+  test "definite nesting not allowed":
+    var output: OutputStream
+    var w = toWriter output
+    writeBytes(w, 1):
+      expect AssertionDefect:
+        writeBytes(w, 1):
+          w.writeByte('a'.byte)
+
+  test "indefinite in definite not allowed":
+    var output: OutputStream
+    var w = toWriter output
+    writeBytes(w, 1):
+      expect AssertionDefect:
+        writeBytes(w):
+          w.writeByte('a'.byte)
+
+  test "text in indefinite bytes not allowed":
+    var output: OutputStream
+    var w = toWriter output
+    writeBytes(w):
+      expect AssertionDefect:
+        writeText(w, 1):
+          w.writeChar('a')
+
+  test "text in definite bytes not allowed":
+    var output: OutputStream
+    var w = toWriter output
+    writeBytes(w, 1):
+      expect AssertionDefect:
+        writeText(w, 1):
+          w.writeChar('a')
+
+  test "non-bytes in indefinite bytes not allowed":
+    var output: OutputStream
+    var w = toWriter output
+    writeBytes(w):
+      expect AssertionDefect:
+        w.write(123)
+
+  test "non-bytes in definite bytes not allowed":
+    var output: OutputStream
+    var w = toWriter output
+    writeBytes(w, 1):
+      expect AssertionDefect:
+        w.write(123)
+
+  test "bytes in definite bytes not allowed":
+    var output: OutputStream
+    var w = toWriter output
+    writeBytes(w, 1):
+      expect AssertionDefect:
+        w.write("a".toBytes)
+
+suite "Test write text/bytes object":
+  test "write StringLikeObj":
+    type ExpectedObj = object
+      definText, indefText: string
+      definBytes, indefBytes: seq[byte]
+      marker: int
+
+    let val = StringLikeObj(
+      definText: "foo".DefinText,
+      indefText: @["bar", "baz"].IndefText,
+      definBytes: "quz".toBytes.DefinBytes,
+      indefBytes: @["qux".toBytes, "quxx".toBytes].IndefBytes,
+      marker: 123,
+    )
+    let cbor = StringLikeCbor.encode(val)
+    #echo cbor.hex
+    checkCbor cbor,
+      ExpectedObj(
+        definText: "foo",
+        indefText: "barbaz",
+        definBytes: "quz".toBytes,
+        indefBytes: "quxquxx".toBytes,
+        marker: 123,
+      )
