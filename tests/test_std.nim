7a5c8ee3e206ce31335baba42eb3f3b5b3a2d29b
nitely <ecastroborsani@gmail.com>
2025-10-03 20:08:56 -0300

    Comptime tests

--- tests/test_std.nim
+++ tests/test_std.nim
@@ -14,14 +14,14 @@ import
   ../cbor_serialization/std/[options, sets, tables]
 
 suite "Test Options":
-  test "some":
+  dualTest "some":
     let val = some(123)
     let cbor = Cbor.encode(val)
     check cbor.hex == "0x187b"
     checkCbor cbor, 123
     checkCbor cbor, val
 
-  test "none":
+  dualTest "none":
     let val = none(int)
     let cbor = Cbor.encode(val)
     check cbor.hex == "0xf6"
@@ -29,48 +29,48 @@ suite "Test Options":
     checkCbor cbor, val
 
 suite "Test Sets":
-  test "set[int8]":
+  dualTest "set[int8]":
     let val = {1'i8, 2, 3}
     let cbor = Cbor.encode(val)
     check cbor.hex == "0x9f010203ff"
     checkCbor cbor, val
 
-  test "set[uint8]":
+  dualTest "set[uint8]":
     let val = {1'u8, 2, 3}
     let cbor = Cbor.encode(val)
     check cbor.hex == "0x9f010203ff"
     checkCbor cbor, val
 
-  test "OrderedSet[int]":
+  dualTest "OrderedSet[int]":
     let val = toOrderedSet([1, 2, 3])
     let cbor = Cbor.encode(val)
     check cbor.hex == "0x9f010203ff"
     checkCbor cbor, val
 
-  test "HashSet[int]":
+  dualTest "HashSet[int]":
     let val = toHashSet([1, 2, 3])
     let cbor = Cbor.encode(val)
     checkCbor cbor, val
 
 suite "Test Tables":
-  test "OrderedTable[int, string]":
+  dualTest "OrderedTable[int, string]":
     let val = toOrderedTable [(1, "one"), (2, "two")]
     let cbor = Cbor.encode(val)
     check cbor.hex == "0xbf6131636f6e6561326374776fff"
     checkCbor cbor, val
 
-  test "OrderedTable[string, string]":
+  dualTest "OrderedTable[string, string]":
     let val = toOrderedTable [("1", "one"), ("2", "two")]
     let cbor = Cbor.encode(val)
     check cbor.hex == "0xbf6131636f6e6561326374776fff"
     checkCbor cbor, val
 
-  test "Table[int, string]":
+  dualTest "Table[int, string]":
     let val = {1: "one", 2: "two"}.toTable
     let cbor = Cbor.encode(val)
     checkCbor cbor, val
 
-  test "Table[string, string]":
+  dualTest "Table[string, string]":
     let val = {"1": "one", "2": "two"}.toTable
     let cbor = Cbor.encode(val)
     checkCbor cbor, val
