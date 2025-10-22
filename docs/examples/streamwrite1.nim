f82c2509b9335f4ff381f56abdea73e8ed44bcc9
Esteban C Borsani <ecastroborsani@gmail.com>
2025-10-22 20:49:33 -0300

    Use nim naming conventions (#10)
    - member -> field
    - record -> object
    - Remove nim-ser aliases

--- docs/examples/streamwrite1.nim
+++ docs/examples/streamwrite1.nim
@@ -5,13 +5,13 @@ var writer = CborWriter[DefaultFlavor].init(output)
 
 # ANCHOR: Nesting
 writer.writeObject:
-  writer.writeMember("status", "ok")
+  writer.writeField("status", "ok")
   writer.writeName("data")
   writer.writeArray:
     for i in 0 ..< 2:
       writer.writeObject:
-        writer.writeMember("id", i)
-        writer.writeMember("name", "item" & $i)
+        writer.writeField("id", i)
+        writer.writeField("name", "item" & $i)
 # ANCHOR_END: Nesting
 
 echo output.getOutput(seq[byte]).to0xHex()
