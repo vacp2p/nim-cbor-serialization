a6bb0a1dcd32cd954592efd6bfd1156bd6f6ed92
nitely <ecastroborsani@gmail.com>
2026-05-27 18:35:56 -0300

    wip

--- tests/test_malformed.nim
+++ tests/test_malformed.nim
@@ -9,36 +9,35 @@
 
 import unittest2, ./utils, ../cbor_serialization
 
+proc checkAny(cbor: seq[byte]) =
+  expect CborNotEnoughBytesError:
+    discard Cbor.decode(cbor, CborValueRef)
+  expect CborNotEnoughBytesError:
+    discard Cbor.decode(cbor, CborBytes)
+  expect CborNotEnoughBytesError:
+    discard Cbor.decode(cbor, CborVoid)
+
 suite "Malformed data tests":
   test "Byte array of len int64.high div 2":
     # byte string of len (int64.high / 2) and content "01020304"
     let cbor = "0x5b3fffffffffffffff01020304".unhex
     expect CborNotEnoughBytesError:
-      discard Cbor.decode(cbor, CborValueRef)
-    expect CborNotEnoughBytesError:
       discard Cbor.decode(cbor, seq[byte])
-    expect CborNotEnoughBytesError:
-      discard Cbor.decode(cbor, CborBytes)
+    checkAny(cbor)
 
   test "String of len int64.high div 2":
     # string of len (int64.high / 2) and content "abcd"
     let cbor = "0x7b3fffffffffffffff61626364".unhex
     expect CborNotEnoughBytesError:
-      discard Cbor.decode(cbor, CborValueRef)
-    expect CborNotEnoughBytesError:
       discard Cbor.decode(cbor, string)
-    expect CborNotEnoughBytesError:
-      discard Cbor.decode(cbor, CborBytes)
+    checkAny(cbor)
 
   test "Array of len int64.high div 2":
     # array of len (int64.high / 2) and content "01020304"
     let cbor = "0x9b3fffffffffffffff01020304".unhex
     expect CborNotEnoughBytesError:
-      discard Cbor.decode(cbor, CborValueRef)
-    expect CborNotEnoughBytesError:
       discard Cbor.decode(cbor, seq[int])
-    expect CborNotEnoughBytesError:
-      discard Cbor.decode(cbor, CborBytes)
+    checkAny(cbor)
 
   test "Map of len int64.high div 2":
     type Obj = object
@@ -47,8 +46,5 @@ suite "Malformed data tests":
     # map of len (int64.high / 2) and content "01020304"
     let cbor = "bb3fffffffffffffff01020304".unhex
     expect CborNotEnoughBytesError:
-      discard Cbor.decode(cbor, CborValueRef)
-    expect CborNotEnoughBytesError:
       discard Cbor.decode(cbor, Obj)
-    expect CborNotEnoughBytesError:
-      discard Cbor.decode(cbor, CborBytes)
+    checkAny(cbor)
